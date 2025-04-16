00c42ba45f61747a869b7b7fc7fc40899329523b
Deep Sengar <128704275+DeepSengar1@users.noreply.github.com>
2025-04-16 17:45:28 +0530

    fixed comments

--- FRONTEND/src/components/feedbacks/Feedbacks.jsx
+++ FRONTEND/src/components/feedbacks/Feedbacks.jsx
@@ -1,25 +1,30 @@
 import { StarIcon } from "lucide-react";
 import React from "react";
 
-const data = [{ username: "deep", rating: 4, comment: "very cold here soo much thandi" },{ username: "Avinash", rating: 2, comment: "very hot here soo much garmi" }];
+const data = [
+  {
+    username: "deep",
+    rating: 4,
+    comment: "very hot here need to increase forest cover",
+  },
+  {
+    username: "Avinash",
+    rating: 2,
+    comment: "local ponds are drying up cause of heat",
+  },
+];
 
 const Feedbacks = () => {
-   const HandleRating = (rating) =>{
-    let i =0;
-    while(rating!==0){
-        i++
-        rating --;
-    }
- 
-   }
   return (
     <div className="flex flex-col gap-2">
       {data.map((dat, i) => {
         return (
           <div key={i} className="bg-cyan-600 px-2 py-0.5 w-92 rounded-xl">
-            <div className="font-bold tracking-wide text-xl">{dat.username}</div>
+            <div className="font-bold tracking-wide text-xl">
+              {dat.username}
+            </div>
             <div className="-m-0.5">{dat.comment} </div>
-            <div>{HandleRating(dat.rating)}</div>
+            <div>rating: {dat.rating}</div>
           </div>
         );
       })}
